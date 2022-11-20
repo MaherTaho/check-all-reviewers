a056e9420712870b0d3913372c4149552b3e193f
MaherTaho <59415575+MaherTaho@users.noreply.github.com>
2022-11-20 15:12:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
 
+const checkAllReviewers = async () => {
+    console.log('Maher Ali 1999');
+};
+checkAllReviewers();
